cac18cca961685a087764966343a9e2b79d11795
Moein <111966284+iammoein@users.noreply.github.com>
2024-01-14 13:34:04 +0330

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const animated_bg_texts = document.querySelectorAll('.animated-bg-text');
 setTimeout(getData,2500);
 
 function getData() {
-  header.innerHTML = '<img src="https://images.unsplash.com/photo-1611162616305-c69b3fa7fbe0?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />';
+  header.innerHTML = '<img src="https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=1575&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />';
   title.innerHTML = 'Lorem ipsum dolor sit amet';
   excerpt.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, ab';
   profile_img.innerHTML = ' <img src="https://randomuser.me/api/portraits/men/46.jpg" alt="">';
